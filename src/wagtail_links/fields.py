thelabnyc/wagtail-links

commit d8a079741ba8c28075fb62c6cef8013534a41038

add description title to link

src/wagtail_links/models.py
@@ -29,8 +29,13 @@ class Link(index.Indexed, models.Model):
     """
     A generic link that points somewhere else using various methods
     """
+    title = models.SlugField(_("Title"),
+        blank=True,
+        help_text=_("Description of link for use in ARIA compliance"))
     name = models.SlugField(_("Name"),
-        unique=True, null=True,
+        unique=True,
+        null=True,
+        blank=True,
         help_text=_("Unique name for this link (for use by Django templates)."))
     link_external = models.URLField(_("External Link"),
         blank=True,

src/wagtail_links/migrations/0005_auto_20191203_1908_squashed_0006_auto_20191204_2129.py
@@ -1,0 +1,52 @@
+# Generated by Django 2.2.8 on 2019-12-04 21:30
+
+import django.core.validators
+from django.db import migrations, models
+import django.db.models.deletion
+import wagtail_links.models
+
+
+class Migration(migrations.Migration):
+
+    replaces = [('wagtail_links', '0005_auto_20191203_1908'), ('wagtail_links', '0006_auto_20191204_2129')]
+
+    dependencies = [
+        ('wagtail_links', '0004_auto_20190411_2102'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='link',
+            options={'verbose_name': 'Link', 'verbose_name_plural': 'Links'},
+        ),
+        migrations.AlterField(
+            model_name='link',
+            name='django_view_name',
+            field=models.CharField(blank=True, help_text='Name of Django view for reverse lookup', max_length=255, validators=[wagtail_links.models.validate_django_reverse], verbose_name='Django View Link'),
+        ),
+        migrations.AlterField(
+            model_name='link',
+            name='link_external',
+            field=models.URLField(blank=True, help_text='Absolute URL Link', verbose_name='External Link'),
+        ),
+        migrations.AlterField(
+            model_name='link',
+            name='link_page',
+            field=models.ForeignKey(blank=True, help_text='Wagtail Page Link', null=True, on_delete=django.db.models.deletion.CASCADE, related_name='+', to='wagtailcore.Page', verbose_name='Wagtail Page Link'),
+        ),
+        migrations.AlterField(
+            model_name='link',
+            name='link_relative',
+            field=models.CharField(blank=True, help_text='Relative URL Link', max_length=200, validators=[django.core.validators.RegexValidator(message='Not valid relative url', regex='^(?!www\\.|(?:http|ftp)s?://|[A-Za-z]:\\\\|//).*')], verbose_name='Relative Link'),
+        ),
+        migrations.AlterField(
+            model_name='link',
+            name='name',
+            field=models.SlugField(blank=True, help_text='Unique name for this link (for use by Django templates).', null=True, unique=True, verbose_name='Name'),
+        ),
+        migrations.AddField(
+            model_name='link',
+            name='title',
+            field=models.SlugField(blank=True, help_text='Description of link for use in ARIA compliance', verbose_name='Title'),
+        ),
+    ]

commit d8d3e21078c179931314203322beb0ca6980c9d6

Fix bug introduced by d8a079741ba8c28075fb62c6cef8013534a41038
Link.name was changed to allow blank entries, but this this incompatible with
the requirement of that field being unique. Django will store empty-string the
the name field is left blank in a form, but two empty-string values would
collide as being unique conflicts.

The solution to this is to intercept empty-string values and convert them to
NULL before being saved in the DB.

src/wagtail_links/fields.py
@@ -1,0 +1,35 @@
+from django.core.exceptions import ImproperlyConfigured
+from django.db.models.fields import SlugField
+
+
+class NullSlugField(SlugField):
+    """
+    SlugField that stores '' as None and returns None as ''
+
+    Useful when using unique=True and forms. Implies null==blank==True.
+    """
+
+    def __init__(self, *args, **kwargs):
+        if not kwargs.get('null', True) or not kwargs.get('blank', True):
+            raise ImproperlyConfigured("NullCharField implies null==blank==True")
+        kwargs['null'] = kwargs['blank'] = True
+        super().__init__(*args, **kwargs)
+
+
+    def from_db_value(self, value, *args, **kwargs):
+        value = self.to_python(value)
+        # If the value was stored as null, return empty string instead
+        return value if value is not None else ''
+
+
+    def get_prep_value(self, value):
+        prepped = super().get_prep_value(value)
+        # If the value was stored as empty string, return None instead
+        return prepped if prepped != "" else None
+
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        del kwargs['null']
+        del kwargs['blank']
+        return name, path, args, kwargs

src/wagtail_links/migrations/0007_auto_20200101_1658.py
@@ -1,0 +1,19 @@
+# Generated by Django 2.2.9 on 2020-01-01 16:58
+
+from django.db import migrations
+import wagtail_links.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wagtail_links', '0006_auto_20200101_1646'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='link',
+            name='name',
+            field=wagtail_links.fields.NullSlugField(help_text='Unique name for this link (for use by Django templates).', unique=True, verbose_name='Name'),
+        ),
+    ]

src/wagtail_links/models.py
@@ -6,6 +6,7 @@ from django.urls import reverse, NoReverseMatch
 from wagtail.admin.edit_handlers import PageChooserPanel, FieldPanel
 from wagtail.search import index
 from wagtail.snippets.models import register_snippet
+from .fields import NullSlugField
 import logging
 
 logger = logging.getLogger(__name__)
@@ -33,10 +34,8 @@ class Link(index.Indexed, models.Model):
         max_length=200,
         blank=True,
         help_text=_("Description of link for use in ARIA compliance"))
-    name = models.SlugField(_("Name"),
+    name = NullSlugField(_("Name"),
         unique=True,
-        null=True,
-        blank=True,
         help_text=_("Unique name for this link (for use by Django templates)."))
     link_external = models.URLField(_("External Link"),
         blank=True,

src/wagtail_links/tests.py
@@ -64,6 +64,13 @@ class WagtailLinksTest(WagtailPageTests):
         self.assertEqual("{}".format(link), "Link[django_view_name=admin:index]: /admin/")
 
 
+    def test_multiple_blank_link_names(self):
+        link1 = Link.objects.create(name='', link_external="https://example.com/1")
+        link2 = Link.objects.create(name='', link_external="https://example.com/2")
+        self.assertEqual(link1.name, '')
+        self.assertEqual(link2.name, '')
+
+
 
 class WagtailLinksTagsTest(WagtailPageTests):
     def test_get_wagtail_link(self):