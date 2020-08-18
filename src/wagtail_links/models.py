c2c94b6a28cff16a86ce0c10eb37de2d77104ffc
Craig Weber <crgwbr@gmail.com>
2020-08-18 15:51:06 -0400

    Replacce ugettext with gettext

--- src/wagtail_links/models.py
+++ src/wagtail_links/models.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.urls import reverse, NoReverseMatch
 from wagtail.admin.edit_handlers import PageChooserPanel, FieldPanel
 from wagtail.search import index
