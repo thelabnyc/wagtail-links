7b215cb3c29c59dd3a33f238decdac1157636889
Craig Weber <crgwbr@gmail.com>
2024-03-21 12:04:31 -0400

    Patch [CVE-2024-21503](https://github.com/advisories/GHSA-fj7x-q9j7-g6q6)

--- sandbox/sandbox/urls.py
+++ sandbox/sandbox/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  re_path(r'^blog/', include('blog.urls'))
 """
+
 from django.urls import re_path, include
 from django.contrib import admin
 from wagtail.admin import urls as wagtailadmin_urls
