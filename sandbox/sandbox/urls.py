0cd3b18dba0b1d24af2ac5eea4558494769f8735
Craig Weber <crgwbr@gmail.com>
2024-06-26 11:22:57 -0400

    Refs #26187 Migrate re_path to path

--- sandbox/sandbox/urls.py
+++ sandbox/sandbox/urls.py
@@ -1,28 +1,12 @@
-"""wagtail_links URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  re_path(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  re_path(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  re_path(r'^blog/', include('blog.urls'))
-"""
-
-from django.urls import re_path, include
+from django.urls import path, include
 from django.contrib import admin
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
 from wagtail import urls as wagtail_urls
 
 urlpatterns = [
-    re_path(r"^admin/", admin.site.urls),
-    re_path(r"^cms/", include(wagtailadmin_urls)),
-    re_path(r"^documents/", include(wagtaildocs_urls)),
-    re_path(r"^pages/", include(wagtail_urls)),
+    path("admin/", admin.site.urls),
+    path("cms/", include(wagtailadmin_urls)),
+    path("documents/", include(wagtaildocs_urls)),
+    path("pages/", include(wagtail_urls)),
 ]
